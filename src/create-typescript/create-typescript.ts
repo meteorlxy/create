a4f1b658e6f692b8491e519dc5b58b5ba9a48043
meteorlxy <meteor.lxy@foxmail.com>
2022-04-15 12:00:12 +0800

    feat: update monorepo tsconfig structure

--- src/create-typescript/create-typescript.ts
+++ src/create-typescript/create-typescript.ts
@@ -18,35 +18,40 @@ export const createTypescript = async (
   );
 
   if (options.monorepo) {
+    // monorepo
     await copy(
       resolve(__dirname, 'templates/monorepo/tsconfig.json'),
       resolve(targetPath, 'tsconfig.json'),
     );
 
     await copy(
-      resolve(__dirname, 'templates/monorepo/packages/foo/tsconfig.json'),
-      resolve(targetPath, 'packages/foo/tsconfig.json'),
+      resolve(__dirname, 'templates/monorepo/tsconfig.build.json'),
+      resolve(targetPath, 'tsconfig.build.json'),
     );
 
-    if (options.eslint) {
-      await copy(
-        resolve(__dirname, 'templates/monorepo/tsconfig.eslint.json'),
-        resolve(targetPath, 'tsconfig.eslint.json'),
-      );
-    }
+    await copy(
+      resolve(__dirname, 'templates/monorepo/packages/foo/tsconfig.build.json'),
+      resolve(targetPath, 'packages/foo/tsconfig.build.json'),
+    );
   } else {
+    // non-monorepo
     await copy(
       resolve(__dirname, 'templates/tsconfig.json'),
       resolve(targetPath, 'tsconfig.json'),
+    );
+
+    await copy(
+      resolve(__dirname, 'templates/tsconfig.build.json'),
+      resolve(targetPath, 'tsconfig.build.json'),
     );
   }
 
   // add scripts & dependencies to package.json
   await extendJson(resolve(targetPath, 'package.json'), {
     scripts: {
-      build: 'tsc -b',
-      clean: 'tsc -b --clean',
-      dev: 'tsc -b --watch',
+      build: 'tsc -b tsconfig.build.json',
+      clean: 'tsc -b --clean tsconfig.build.json',
+      dev: 'tsc -b --watch tsconfig.build.json',
     },
     devDependencies: await getPackagesVersion([
       '@meteorlxy/tsconfig',
